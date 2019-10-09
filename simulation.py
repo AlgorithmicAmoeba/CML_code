0cd11d2b9b6ba54c64898d268e924ce9513907bf
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-09 09:54:15 +0200

    Make state transition function use exogenous inputs

--- simulation.py
+++ simulation.py
@@ -49,7 +49,7 @@ ts = numpy.linspace(0, list(glucose['Time'])[-1], 1000)
 dt = ts[1]
 # Biomass C H_1.8 O_0.5 N_0.2 => 24.6 g/mol
 #     Ng, Nx, Nfa, Ne, Nco, No, Nn, Na, Nb, Nz, Ny, V, Vg
-X0 = [0, 4.6/24.6, 0, 0, 0, 0, 0, 1e-5, 0, 5.1, 1.2, 1.077, 0.1]
+X0 = numpy.array([0, 4.6/24.6, 0, 0, 0, 0, 0, 1e-5, 0, 5.1, 1.2, 1.077, 0.1])
 
 m = Model(X0)
 Xs = [m.outputs()]
@@ -61,25 +61,52 @@ def hx(x):
     return Ng/V, Nfa/V, Ne/V
 
 
-def fx(x, dt):
-    ts = numpy.linspace(0, dt, 1000)
-    dt_small = ts[1]
-    m = Model(x)
-    for _ in ts:
-        m.step(inputs, dt_small)
-    return m.X
+class fx_obj:
+    def __init__(self, t=0):
+        self.t = t
+
+    def __call__(self, x, dt):
+        ts = numpy.linspace(0, dt, 100)
+        dt_small = ts[1]
+        m_f = Model(x)
+        m_f.t = self.t
+        for _ in ts:
+            m_f.step(inputs, dt_small)
+        return m_f.X
 
 
+fx = fx_obj()
 nx = len(X0)
-Q = numpy.diag(numpy.full(nx, 1))
-R = numpy.diag(numpy.full(3, 0.5))
-sigmas = filterpy.kalman.JulierSigmaPoints(nx)
+Q = numpy.diag(numpy.array([1.e-06, 1.e-05, 1.e-05, 1.e-06, 1.e-05, 1.e-05, 1.e-05, 1.e-05,
+       1.e-05, 1.e-05, 1.e-05, 1.e-05, 1.e-05]))
+R = numpy.diag(numpy.full(3, 1e-5))
+sigmas = filterpy.kalman.MerweScaledSigmaPoints(nx, 1e-3, 2, 0)
 ukf = filterpy.kalman.UnscentedKalmanFilter(nx, 3, 6 * 3600, hx, fx, sigmas)
+ukf.x = X0.copy()
 ukf.Q = Q
 ukf.R = R
 
+ts_meas = concentration['Time']
+Cg_meas, Cfa_meas, Ce_meas = concentration['Glucose'], concentration['Fumaric'], concentration['Ethanol']
+
+t_old_meas = 0
+ind_next_measure = 1
+t_next_meas = ts_meas[ind_next_measure]
 for ti in tqdm.tqdm(ts[1:]):
     Xs.append(list(m.step(inputs, dt)))
+
+    if ti > t_next_meas:
+        ukf.predict(t_next_meas - t_old_meas)
+        print('p', ukf.x)
+        z = [Ci[ind_next_measure] for Ci in [Cg_meas/180, Cfa_meas/116, Ce_meas/46]]
+        ukf.update(z)
+        print('u', ukf.x)
+        m.X = ukf.x.copy()
+
+        t_old_meas = t_next_meas
+        ind_next_measure += 1
+        t_next_meas = ts_meas[ind_next_measure]
+        fx.t = ti
 
 Xs = numpy.array(Xs)
 
@@ -94,17 +121,17 @@ pH = Xs[:, 13]
 plt.figure(figsize=(20, 20))
 plt.subplot(2, 2, 1)
 plt.plot(ts, Cgs)
-plt.plot(concentration['Time'], concentration['Glucose'], '.')
+plt.plot(ts_meas, Cg_meas, '.')
 plt.title("Glucose")
 
 plt.subplot(2, 2, 2)
 plt.plot(ts, Cfas)
-plt.plot(concentration['Time'], concentration['Fumaric'], '.')
+plt.plot(ts_meas, Cfa_meas, '.')
 plt.title("Fumaric")
 
 plt.subplot(2, 2, 3)
 plt.plot(ts, Ces)
-plt.plot(concentration['Time'], concentration['Ethanol'], '.')
+plt.plot(ts_meas, Ce_meas, '.')
 plt.title("Ethanol")
 
 plt.subplot(2, 2, 4)
