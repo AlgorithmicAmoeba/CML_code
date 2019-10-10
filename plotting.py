bdf39961ded0a484097fa5c3a44f96fe697137aa
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-10 16:52:06 +0200

    Separate simulation execution and plotting

---
+++ plotting.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import pandas
+import scipy.stats
+
+
+def plot_all(file_name, confidence=0.95, show=True):
+    xls = pandas.ExcelFile(file_name)
+    model = pandas.read_excel(xls, 'model')
+    se = pandas.read_excel(xls, 'se')
+    su = pandas.read_excel(xls, 'su')
+
+    # SE means
+    ts = se['ts']
+    Vs = se['V']
+    Cgs = se['Ng'] * 180 / Vs
+    Cfas = se['Nfa'] * 116 / Vs
+    Ces = se['Ne'] * 46 / Vs
+    Czs = se['Nz'] / Vs
+    Cys = se['Ny'] / Vs
+
+    # Standard devioation multiplier to get the correct confidence interval
+    K = scipy.stats.norm.ppf(0.95)
+    # SE covs
+    Pgs = se['Ng_cov'] * 180 / Vs * K
+    Pfas = se['Nfa_cov'] * 116 / Vs * K
+    Pes = se['Ne_cov'] * 46 / Vs * K
+    Pzs = se['Nz_cov'] / Vs * K
+    Pys = se['Ny_cov'] / Vs * K
+
+    # Measured update values
+    ts_meas = su['ts']
+    Cg_meas = su['Cg']
+    Cfa_meas = su['Cfa']
+    Ce_meas = su['Ce']
+
+    plt.figure(figsize=(20, 20))
+    plt.subplot(2, 2, 1)
+    plt.plot(ts, Cgs + Pgs)
+    plt.plot(ts, Cgs - Pgs)
+    plt.plot(ts_meas, Cg_meas, '.')
+    plt.title("Glucose")
+
+    plt.subplot(2, 2, 2)
+    plt.plot(ts, Cfas + Pfas)
+    plt.plot(ts, Cfas - Pfas)
+    plt.plot(ts_meas, Cfa_meas, '.')
+    plt.title("Fumaric")
+
+    plt.subplot(2, 2, 3)
+    plt.plot(ts, Ces + Pes)
+    plt.plot(ts, Ces - Pes)
+    plt.plot(ts_meas, Ce_meas, '.')
+    plt.title("Ethanol")
+
+    plt.subplot(2, 2, 4)
+    plt.plot(ts, Czs + Pzs, label="Z+")
+    plt.plot(ts, Czs - Pzs, label="Z-")
+
+    plt.plot(ts, Cys + Pys, label="Y+")
+    plt.plot(ts, Cys - Pys, label="Y-")
+    plt.title("Enzyme")
+    plt.legend()
+
+    if show:
+        plt.show()
+
+    # plt.plot(ts, pH)
+    # plt.show()
+
+
+# plot_all('data/result.xls')
--- simulation.py
+++ simulation.py
@@ -1,23 +1,23 @@
 import pandas
 import numpy
-import matplotlib.pyplot as plt
 from model import Model
 import StateEstimator
 import inputters
 import stateUpdaters
 import tqdm
+import plotting
 
 
 inputs = inputters.FakeInputs("data/run_9_glucose.csv")
 su = stateUpdaters.FakeStateUpdate("data/run_9_conc.csv")
 
-# ts = numpy.linspace(0, 200, 200)
-ts = numpy.linspace(0, 30, 60)
+ts = numpy.linspace(0, 200, 200)
+
 # Biomass C H_1.8 O_0.5 N_0.2 => 24.6 g/mol
 #     Ng, Nx, Nfa, Ne, Nco, No, Nn, Na, Nb, Nz, Ny, V, Vg
 X0 = [0, 4.6/24.6, 0, 0, 0, 0, 0, 1e-5, 0, 5.1, 1.2, 1.077, 0.1]
 
-m = Model(X0, inputs)
+m = Model(X0, inputs, pH_calculations=True)
 Xs = [X0]
 
 # State estimation
@@ -46,48 +46,10 @@ se_data.index.name = 'ts'
 se_data.to_excel(xls, 'se')
 
 su_names = ['Cg', 'Cfa', 'Ce']
-su_data = pandas.DataFrame(su.get_data().values(), index=su.get_times(), columns=su_names)
+su_data = pandas.DataFrame(su.get_data().values, index=su.get_times(), columns=su_names)
 su_data.index.name = 'ts'
 su_data.to_excel(xls, 'su')
 
 xls.save()
 
-Vs = Xs[:, 11]
-Cgs = Xs[:, 0] * 180 / Vs
-Cfas = Xs[:, 2] * 116 / Vs
-Ces = Xs[:, 3] * 46 / Vs
-Czs = Xs[:, 9] / Vs
-Cys = Xs[:, 10] / Vs
-# pH = Xs[:, 13]
-
-Pgs = se.get_Ps()[:, 0] * 180 / Vs
-
-plt.figure(figsize=(20, 20))
-plt.subplot(2, 2, 1)
-plt.plot(ts, Cgs + Pgs)
-plt.plot(ts, Cgs - Pgs)
-plt.plot(su.ts_meas, su.Cg_meas, '.')
-plt.title("Glucose")
-
-plt.subplot(2, 2, 2)
-plt.plot(ts, Cfas)
-plt.plot(su.ts_meas, su.Cfa_meas, '.')
-plt.title("Fumaric")
-
-plt.subplot(2, 2, 3)
-plt.plot(ts, Ces)
-plt.plot(su.ts_meas, su.Ce_meas, '.')
-plt.title("Ethanol")
-
-plt.subplot(2, 2, 4)
-plt.plot(ts, Czs, label="Z")
-plt.plot(ts, Cys, label="Y")
-plt.title("Enzyme")
-plt.legend()
-
-plt.show()
-
-# plt.plot(ts, pH)
-# plt.show()
-
-
+plotting.plot_all('data/result.xls')
