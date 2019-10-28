6bc8f3706f9f7bd61c6af3cc38f92ee1d5d1e71b
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-28 14:43:00 +0200

    Add simulation objects

--- labview.py
+++ labview.py
@@ -1,10 +1,22 @@
 # Contains all functions that labview will call
 import inputters
+import stateUpdaters
+import Model
+import StateEstimator
 
 
 class Labview:
     def __init__(self):
         self.inputs = inputters.LabviewInputs()
+        self.su = stateUpdaters.LabviewStateUpdate()
+        self.X0 = [0, 4.6/24.6, 0, 0, 0, 0, 0, 1e-5, 0, 5.1, 1.2, 1.077, 0.1, 25]
+
+        self.m = Model.Model(self.X0, self.inputs, pH_calculations=True)
+        self.Xs = [self.X0]
+
+        # State estimation
+        self.t_predict = 1
+        self.se = StateEstimator.StateEstimator(self.X0, self.inputs, self.t_predict)
 
 
 lv = Labview()
