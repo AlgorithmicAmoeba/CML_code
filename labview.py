050263bd94de0902e437015b6a69a483a94b90b0
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-29 11:07:22 +0200

    Build labview interface

--- labview.py
+++ labview.py
@@ -3,10 +3,14 @@ import inputters
 import stateUpdaters
 import Model
 import StateEstimator
+import scipy.stats
 
 
 class Labview:
     def __init__(self):
+        self.t = 0
+        self.ts = [self.t]
+
         self.inputs = inputters.LabviewInputs()
         self.su = stateUpdaters.LabviewStateUpdate()
         self.X0 = [0, 4.6/24.6, 0, 0, 0, 0, 0, 1e-5, 0, 5.1, 1.2, 1.077, 0.1, 25]
@@ -52,8 +56,49 @@ def update_inputs(t, inputs):
     lv.inputs.update(t, inputs)
 
 
-def get_model_outputs():
+def step(t):
+    dt = t - lv.t
+    lv.ts.append(t)
+    lv.m.step(dt)
+    lv.se.step(dt)
+    lv.su.step(dt)
+    if lv.su.update_ready():
+        t_u, z = lv.su.get_update()
+        lv.se.update(z, t-t_u)
+
+
+def update_state(t, z):
+    lv.su.update = True
+    lv.su.update_value = z
+    lv.su.update_time = t
+
+
+def get_glucose_graph(confidence=0.95):
     """
     Passes outputs to labview from the model
     """
-    pass
+    model = lv.m.get_data()
+    se = lv.se.get_data()
+    su = lv.su.get_data().values
+
+    ts = lv.ts
+    # Model
+    Vs_m = model[:, 11]
+    Cgs_m = model[:, 0] * 180 / Vs_m
+
+    # State estimator
+    Vs = se[:, 11]
+    Cgs = se[:, 0] * 180 / Vs_m
+
+    # Standard deviation multiplier to get the correct confidence interval
+    K = scipy.stats.norm.ppf(confidence)
+    # SE covs
+    Pgs = se[:, 14 + 0] * 180 / Vs * K
+
+    # Measured update values
+    ts_meas = lv.su.get_times()
+    Cg_meas = su[:, 0][:len(ts_meas)]
+
+    plots = [(ts, Cgs_m), (ts, Cgs + Pgs), (ts, Cgs - Pgs), (ts_meas, Cg_meas)]
+
+    return plots
