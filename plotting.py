4d7cb97bdf7b016e75d384fcf968c0393f8fc2c3
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-11-22 13:27:23 +0200

    Improve plotting functionality

--- plotting.py
+++ plotting.py
@@ -51,48 +51,55 @@ def plot_all(file_name, confidence=0.95, show=True):
     Ce_meas = su['Ce']
 
     plt.figure(figsize=(20, 20))
-    plt.subplot(3, 2, 1)
+    plt.rc("font", size=20)
+    plt.subplot(2, 2, 1)
     plt.plot(ts_m, Cgs_m, "--")
-    plt.plot(ts, Cgs + Pgs)
-    plt.plot(ts, Cgs - Pgs)
+    plt.plot(ts_m, Cgs, "-.")
+    plt.plot(ts, Cgs + Pgs, 'tab:purple')
+    plt.plot(ts, Cgs - Pgs, 'tab:purple')
     plt.plot(ts_meas, Cg_meas, '.')
     plt.title("Glucose")
 
-    plt.subplot(3, 2, 2)
+    plt.subplot(2, 2, 2)
     plt.plot(ts_m, Cfas_m, "--")
-    plt.plot(ts, Cfas + Pfas)
-    plt.plot(ts, Cfas - Pfas)
+    plt.plot(ts_m, Cfas, "-.")
+    plt.plot(ts, Cfas + Pfas, 'tab:purple')
+    plt.plot(ts, Cfas - Pfas, 'tab:purple')
     plt.plot(ts_meas, Cfa_meas, '.')
     plt.title("Fumaric")
 
-    plt.subplot(3, 2, 3)
+    plt.subplot(2, 2, 3)
     plt.plot(ts_m, Ces_m, "--")
-    plt.plot(ts, Ces + Pes)
-    plt.plot(ts, Ces - Pes)
+    plt.plot(ts_m, Ces, "-.")
+    plt.plot(ts, Ces + Pes, 'tab:purple')
+    plt.plot(ts, Ces - Pes, 'tab:purple')
     plt.plot(ts_meas, Ce_meas, '.')
     plt.title("Ethanol")
 
-    plt.subplot(3, 2, 4)
+    plt.subplot(2, 2, 4)
     plt.plot(ts_m, Czs_m, "--")
-    plt.plot(ts, Czs + Pzs, label="Z+")
-    plt.plot(ts, Czs - Pzs, label="Z-")
+    plt.plot(ts_m, Czs, "-.")
+    plt.plot(ts, Czs + Pzs, 'tab:purple', label="Z")
+    plt.plot(ts, Czs - Pzs, 'tab:purple')
 
     plt.plot(ts_m, Cys_m, "--")
-    plt.plot(ts, Cys + Pys, label="Y+")
-    plt.plot(ts, Cys - Pys, label="Y-")
+    plt.plot(ts_m, Cys_m, "-.")
+    plt.plot(ts, Cys + Pys, 'tab:pink', label="Y")
+    plt.plot(ts, Cys - Pys, 'tab:pink')
     plt.title("Enzyme")
     plt.legend()
 
-    plt.subplot(3, 2, 5)
-    plt.plot(ts_m, Ts_m, "--")
-    plt.plot(ts, Ts + PTs)
-    plt.plot(ts, Ts - PTs)
-    plt.title("Temperature")
+    # plt.subplot(3, 2, 5)
+    # plt.plot(ts_m, Ts_m, "--")
+    # plt.plot(ts, Ts + PTs)
+    # plt.plot(ts, Ts - PTs)
+    # plt.title("Temperature")
+    #
+    # plt.subplot(3, 2, 6)
+    # plt.plot(ts_m, pH_m)
+    # plt.title("pH")
 
-    plt.subplot(3, 2, 6)
-    plt.plot(ts_m, pH_m)
-    plt.title("pH")
-
+    plt.savefig("results/all9.pdf")
     if show:
         plt.show()
 
@@ -224,5 +231,58 @@ def plot_data(file_name, show=True):
         plt.show()
 
 
+# noinspection DuplicatedCode
+def plot_model(file_name,  show=True):
+    xls = pandas.ExcelFile(file_name)
+    model = pandas.read_excel(xls, 'model')
+    se = pandas.read_excel(xls, 'se')
+    su = pandas.read_excel(xls, 'su')
+
+    # Model
+    ts_m = model['ts']
+    Vs_m = model['V']
+    Cgs_m = model['Ng'] * 180 / Vs_m
+    Cfas_m = model['Nfa'] * 116 / Vs_m
+    Ces_m = model['Ne'] * 46 / Vs_m
+    Czs_m = model['Nz'] / Vs_m
+    Cys_m = model['Ny'] / Vs_m
+    Ts_m = model['T']
+    pH_m = model['pH']
+
+    # Measured update values
+    ts_meas = su['ts']
+    Cg_meas = su['Cg']
+    Cfa_meas = su['Cfa']
+    Ce_meas = su['Ce']
+
+    plt.figure(figsize=(20, 20))
+    plt.rc("font", size=20)
+    plt.subplot(2, 2, 1)
+    plt.plot(ts_m, Cgs_m, "--")
+    plt.plot(ts_meas, Cg_meas, '.')
+    plt.title("Glucose")
+
+    plt.subplot(2, 2, 2)
+    plt.plot(ts_m, Cfas_m, "--")
+    plt.plot(ts_meas, Cfa_meas, '.')
+    plt.title("Fumaric")
+
+    plt.subplot(2, 2, 3)
+    plt.plot(ts_m, Ces_m, "--")
+    plt.plot(ts_meas, Ce_meas, '.')
+    plt.title("Ethanol")
+
+    plt.subplot(2, 2, 4)
+    plt.plot(ts_m, Czs_m, "--", label="Z")
+
+    plt.plot(ts_m, Cys_m, "--", label="Y")
+    plt.title("Enzyme")
+    plt.legend()
+
+    plt.savefig("results/model7.pdf")
+    if show:
+        plt.show()
+
+
 if __name__ == "__main__":
-    plot_all('results/result.xls')
+    plot_all('results/result.xlsx')
