4c8352e3e1d5682f258857a0b6f5d4a6e3906759
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-11-23 09:06:12 +0200

    Add span and bias for binary inputs

--- inputters.py
+++ inputters.py
@@ -64,8 +64,8 @@ class LabviewInputs:
         self.inputs.append(data)
 
     def __call__(self, t):
-        offsets = [0.004422699015892, 0.004053967439397, 0.004, 0.004, 0.004, 0.004, 0.004, 0.004, 0.004, 0.004]
-        spans = [0.0004717700792543, 9.925512172352e-5, 4e-5, 0.000542, 4e-5, 0.00032, 0.00064, 0.0008]
+        offsets = [0.004422699015892, 0.004053967439397, 0.004, 0.004, 0.004, 0.004, 0.004, 0.004, 0.004, 0.004, 0, 0]
+        spans = [0.0004717700792543, 9.925512172352e-5, 4e-5, 0.000542, 4e-5, 0.00032, 0.00064, 0.0008, 1, 1]
 
         index = min(numpy.searchsorted(self.ts, t), len(self.ts)-1)
         ins = [(in_i - offset)/span for in_i, offset, span in zip(self.inputs[index], offsets, spans)]
