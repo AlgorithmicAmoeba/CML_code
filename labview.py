09b1f70edbe74e34f8ec62e8a51a87ce1455024a
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-11-24 09:35:58 +0200

    Add docs

--- labview.py
+++ labview.py
@@ -9,13 +9,48 @@ import plotting
 
 
 class Labview:
+    """Contains the required objects to store state for labview functions
+
+    Attributes
+    ----------
+    t : float
+        The current time
+
+    ts : array_like
+        List of times
+
+    inputs : inputters.LabviewInputs
+        Input object that stores and retrieves inputs
+
+    su : stateUpdaters.LabviewStateUpdate
+        State update object that stores and retrieves HPLC input data
+
+    X0 : array_like
+        The initial state
+
+    m : Model.Model
+        The system model
+
+    Xs : array_like
+        List of previous states
+
+    t_predict : float
+        The period between state predictions
+
+    se : StateEstimator.StateEstimator
+        State estimator object
+
+    live_plot : bool
+        If `True` then a live plot of the run is shown
+
+    """
     def __init__(self):
         self.t = 0
         self.ts = [self.t]
 
         self.inputs = inputters.LabviewInputs()
         self.su = stateUpdaters.LabviewStateUpdate()
-        self.X0 = [0, 4.6/24.6, 0, 0, 0, 0, 0, 1e-5, 0, 5.1, 1.2, 1.077, 0.1, 25]
+        self.X0 = [0, 4.6/24.6, 0, 0, 0, 0, 0, 1e-5, 0, 5.1, 1.2, 1.077, 0.1, 35]
 
         self.m = Model.Model(self.X0, self.inputs, pH_calculations=True)
         self.Xs = [self.X0]
@@ -36,24 +71,22 @@ if lv.live_plot:
 
 
 def init():
-    """
-    Initialises the labview interface.
+    """ Initialises the labview interface.
     Called before the while loop in labview
     """
     pass
 
 
 def finalise():
-    """
-    Ends the labview interface.
+    """Ends the labview interface.
     Called after the while loop in labview
     """
     pass
 
 
 def update_inputs(t, inputs):
-    """
-    Passes inputs into the system to the model.
+    """ Passes inputs into the system to the model.
+
     Parameters
     ----------
     t : float
@@ -66,6 +99,13 @@ def update_inputs(t, inputs):
 
 
 def step(t):
+    """Steps the labview object through time
+
+    Parameters
+    ----------
+    t : float
+        Current time
+    """
     dt = t - lv.t
     lv.t = t
     lv.ts.append(t)
@@ -81,14 +121,28 @@ def step(t):
 
 
 def update_state(t, z):
+    """Sends update to the state updater
+
+    Parameters
+    ----------
+    t : float
+        Timestamp of the update
+
+    z : array_like
+        Update values
+    """
     lv.su.update = True
     lv.su.update_value = [zi/n for zi, n in zip(z, [180, 116, 46])]
     lv.su.update_time = t
 
 
 def get_glucose_graph(confidence=0.95):
-    """
-    Passes outputs to labview from the model
+    """ Passes outputs to labview from the model
+
+    Parameters
+    ----------
+    confidence : float
+        The confidence probability for the plots
     """
     model = lv.m.get_data()
     se = lv.se.get_data()
