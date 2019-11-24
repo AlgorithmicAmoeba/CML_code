c076de6e8a77f29e749dc87e7c19ee2822a2ee88
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-11-24 09:09:14 +0200

    Add docs

--- inputters.py
+++ inputters.py
@@ -3,6 +3,18 @@ import pandas
 
 
 class FakeInputs:
+    """Creates fake inputs for the glucose feed from past data
+
+    Parameters
+    -----------
+    glucose_data_file : string
+        The name of the csv file that contains the glucose data
+
+    Attributes
+    -----------
+    glucose : pandas.Dataframe
+        An object containing the info from the glucose file
+    """
     def __init__(self, glucose_data_file):
         self.glucose = pandas.read_csv(glucose_data_file)
 
@@ -35,10 +47,60 @@ class FakeInputs:
         return Fg_in, Cg_in, Fco_in, Cco_in, Fo_in, Co_in, Fg_out, Cn_in, Fn_in, Fb_in, Cb_in, Fm_in, F_out, T_amb, Q
 
     def CgFg(self, t):
+        """Interpolates the value from the glucose file
+
+        Parameters
+        ----------
+        t : float
+            The value of time at which the input should be looked up
+        """
         return numpy.interp(t, self.glucose['Time'], self.glucose['Glucose dosing (g/h)'])  # g/h
 
 
 class LabviewInputs:
+    """Stores and looks up input values from Labview
+
+    Attributes
+    -----------
+    ts : array_like
+        Stores the time stamp information about the inputs
+
+    inputs : array_like
+        Stores the inputs
+
+    Cg_in : float, constant
+        The glucose feed concentration
+
+    G_rpm_to_ml_min : float, constant
+        The conversion factor between rpm and ml/min for the glucose pump
+
+    Cco_in : float, constant
+        The percentage CO2 in the CO2 feed
+
+    Co_in : float, constant
+        The percentage O2 in the O2 feed
+
+    Cn_in : float, constant
+        The concentration urea in the nitrogen feed
+
+    N_rpm_to_ml_min : float, constant
+        The conversion factor between rpm and ml/min for the nitrogen pump
+
+    B_rpm_to_ml_min : float, constant
+        The conversion factor between rpm and ml/min for the sodium hydroxide pump
+
+    Cb_in : float, constant
+        The concentration of sodium hydroxide in the base feed
+
+    M_rpm_to_ml_min : float, constant
+        The conversion factor between rpm and ml/min for the mineral pump
+
+    T_amb : float
+        The ambient room temperature
+
+    Q_fact : float, constant
+        A multiplier for the heater gain
+    """
     def __init__(self):
         self.ts = []
         self.inputs = []
@@ -60,6 +122,15 @@ class LabviewInputs:
         self.Q_fact = 1
 
     def update(self, t, data):
+        """Update the current inputss
+
+        Parameters
+        ----------
+        t : float
+            Current time
+        data : array_like
+            Current inputs
+        """
         self.ts.append(t)
         self.inputs.append(data)
 
@@ -99,6 +170,13 @@ class LabviewInputs:
         return Fg_in, Cg_in, Fco_in, Cco_in, Fo_in, Co_in, Fg_out, Cn_in, Fn_in, Fb_in, Cb_in, Fm_in, F_out, T_amb, Q
 
     def get_data(self):
+        """Get all the input data
+
+        Returns
+        -------
+        out : array_like
+            All the input data
+        """
         ts = numpy.array(self.ts)
         inputs = numpy.array(self.inputs)
         out = numpy.concatenate([ts, inputs])
